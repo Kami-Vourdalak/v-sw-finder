df227e7468ce2acdc2dab09219e957d6dff5c340
sergio.touset <sergiotl@ext.bershka.com>
2022-08-24 17:05:58 +0200

    feat: add basic layout

---
+++ src/components/AppLayout/AppLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-screen w-full bg-neutral-900 text-gray-50">
+    <div class="flex justify-center items-center h-24 pl-4">
+      <h1 class="text-3xl font-bold">Star Wars Finder</h1>
+    </div>
+    <slot />
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "TheLayout",
+};
+</script>
